7370c3a0f3780f3f7c5d36ff625a58ed60b8d842
mendesdavi <davi.aviva@gmail.com>
2018-05-29 14:59:14 -0300

    SpO2 Calc

---
+++ MAX30100_SpO2Calculator.c
@@ -0,0 +1,55 @@
+/*
+ * MAX30100_SpO2Calculator.c
+ *
+ *  Created on: 29 de mai de 2018
+ *      Author: Davi Mendes
+ */
+
+
+#include <math.h>
+
+#include "MAX30100_SpO2Calculator.h"
+
+// SaO2 Look-up Table
+// http://www.ti.com/lit/an/slaa274b/slaa274b.pdf
+const uint8_t SpO2Calculator::spO2LUT[43] = {100,100,100,100,99,99,99,99,99,99,98,98,98,98,
+                                             98,97,97,97,97,97,97,96,96,96,96,96,96,95,95,
+                                             95,95,95,95,94,94,94,94,94,93,93,93,93,93};
+
+uint8_t spO2CalcgetSpO2()
+{
+    return spO2;
+}
+
+void spO2Calcreset()
+{
+    samplesRecorded = 0;
+    redACValueSqSum = 0;
+    irACValueSqSum = 0;
+    beatsDetectedNum = 0;
+    spO2 = 0;
+}
+
+void spO2Calcupdate(float irACValue, float redACValue, bool beatDetected)
+{
+    irACValueSqSum += irACValue * irACValue;
+    redACValueSqSum += redACValue * redACValue;
+    ++samplesRecorded;
+
+    if (beatDetected) {
+        ++beatsDetectedNum;
+        if (beatsDetectedNum == CALCULATE_EVERY_N_BEATS) {
+            float acSqRatio = 100.0 * log(redACValueSqSum/samplesRecorded) / log(irACValueSqSum/samplesRecorded);
+            uint8_t index = 0;
+
+            if (acSqRatio > 66) {
+                index = (uint8_t)acSqRatio - 66;
+            } else if (acSqRatio > 50) {
+                index = (uint8_t)acSqRatio - 50;
+            }
+            reset();
+
+            spO2 = spO2LUT[index];
+        }
+    }
+}
---
+++ MAX30100_SpO2Calculator.h
@@ -0,0 +1,27 @@
+/*
+ * MAX30100_SpO2Calculator.h
+ *
+ *  Created on: 29 de mai de 2018
+ *      Author: Davi Mendes
+ */
+
+#ifndef MAX30100_SPO2CALCULATOR_H_
+#define MAX30100_SPO2CALCULATOR_H_
+
+#include <stdint.h>
+
+#define CALCULATE_EVERY_N_BEATS         3
+
+void spO2Calcupdate(float irACValue, float redACValue, bool beatDetected);
+void spO2Calcreset();
+uint8_t spO2CalcgetSpO2();
+
+static const uint8_t spO2LUT[43];
+
+float irACValueSqSum;
+float redACValueSqSum;
+uint8_t beatsDetectedNum;
+uint32_t samplesRecorded;
+uint8_t spO2;
+
+#endif /* MAX30100_SPO2CALCULATOR_H_ */
